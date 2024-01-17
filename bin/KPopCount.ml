1e3ec7871c064d76e72258005bc2bfd18955e29f
Paolo Ribeca <paolo.ribeca@gmail.com>
2024-01-17 11:13:03 +0000

    ***BREAKING CHANGE*** Reworked hashing modes for files in KPopCount - default is now single-stranded for FASTA DNA inputs and double-stranded for FASTQ DNA inputs. Also improved verbose output

--- bin/KPopCount.ml
+++ bin/KPopCount.ml
@@ -39,8 +39,8 @@ module KMerCounter (KMH: KMers.Hash_t with type t = int):
                 let min_binding =
                   KMers.HashFrequencies.Base.fold (fun _ occs old_min -> min old_min !occs) res max_int in
                 if verbose then
-                  Printf.eprintf "\rKMerCounter.compute: Outputting and removing hashes having #%d occurrences...%!"
-                    min_binding;
+                  Printf.eprintf "%s\r(%s): Outputting and removing hashes having #%d occurrences...%!"
+                    Tools.String.TermIO.clear __FUNCTION__ min_binding;
                 KMers.HashFrequencies.Base.filter_map_inplace
                   (fun hash occs ->
                     if !occs = min_binding then begin
@@ -55,14 +55,14 @@ module KMerCounter (KMH: KMers.Hash_t with type t = int):
                   Printf.eprintf " done.\n%!"
               end)
             read.seq;
-          if verbose && !reads_cntr mod 10_000 = 0 then
-            Printf.eprintf "\rKMerCounter.compute: Added %d reads%!" !reads_cntr;
+          if verbose && !reads_cntr mod 1_000 = 0 then
+            Printf.eprintf "%s\r(%s): Added %d reads%!" Tools.String.TermIO.clear __FUNCTION__ !reads_cntr;
           if segm_id = 0 then
             incr reads_cntr)
         store;
       if verbose then begin
-        Printf.eprintf "\rKMerCounter.compute: Added %d reads\n%!" !reads_cntr;
-        Printf.eprintf "KMerCounter.compute: Outputting hashes...%!";
+        Printf.eprintf "%s\r(%s): Added %d reads.\n%!" Tools.String.TermIO.clear __FUNCTION__ !reads_cntr;
+        Printf.eprintf "(%s): Outputting hashes...%!" __FUNCTION__;
       end;
       KMers.HashFrequencies.iter (Printf.fprintf output output_format) res;
       if verbose then
@@ -73,21 +73,20 @@ module KMerCounter (KMH: KMers.Hash_t with type t = int):
 module Content =
   struct
     type t =
-      | DNA
+      | DNA_ss
+      | DNA_ds
       | Protein
     exception Invalid_content of string
     let of_string = function
-      | "DNA" | "dna" -> DNA
+      | "DNA-ss" | "DNA-single-stranded" -> DNA_ss
+      | "DNA-ds" | "DNA-double-stranded" -> DNA_ds
       | "protein" | "prot" -> Protein
       | w -> Invalid_content w |> raise
-    let to_string = function
-      | DNA -> "DNA"
-      | Protein -> "protein"
   end
 
 module Parameters =
   struct
-    let content = ref Content.DNA
+    let content = ref None
     let k = ref 12
     let max_results_size = ref 16777216 (* Or: 4^12 *)
     let inputs = ref []
@@ -99,8 +98,8 @@ module Parameters =
 
 let info = {
   Tools.Info.name = "KPopCount";
-  version = "9";
-  date = "02-Jan-2024"
+  version = "10";
+  date = "17-Jan-2024"
 } and authors = [
   "2017-2024", "Paolo Ribeca", "paolo.ribeca@gmail.com"
 ]
@@ -116,7 +115,7 @@ let () =
       [ "k-mer length"; "(must be positive, and <= 30 for DNA or <= 12 for protein)" ],
       TA.Default (fun () -> string_of_int !Parameters.k),
       (fun _ -> Parameters.k := TA.get_parameter_int_pos ());
-    [ "-m"; "-M"; "--max-results-size" ],
+    [ "-M"; "--max-results-size" ],
       Some "<positive_integer>",
       [ "maximum number of k-mer signatures to be kept in memory at any given time.";
         "If more are present, the ones corresponding to the lowest cardinality";
@@ -125,24 +124,32 @@ let () =
       TA.Default (fun () -> string_of_int !Parameters.max_results_size),
       (fun _ -> Parameters.max_results_size := TA.get_parameter_int_pos ());
     TA.make_separator "Input/Output";
-    [ "-c"; "-C"; "--content"; "--mode" ],
-      Some "'DNA'|'protein'",
-      [ "how file contents should be interpreted" ],
-      TA.Default (fun () -> Content.to_string !Parameters.content),
-      (fun _ -> Parameters.content := TA.get_parameter () |> Content.of_string);
-    [ "-f"; "-F"; "--fasta" ],
+    [ "-C"; "--content" ],
+      Some "'DNA-ss'|'DNA-single-stranded'|'DNA-ds'|'DNA-double-stranded'|'protein'",
+      [ "how file contents should be interpreted.";
+        "When content is 'DNA-ss' or 'protein', the sequence is hashed;";
+        "when content is 'DNA-ds', both sequence and reverse complement are hashed" ],
+      TA.Default (fun _ -> "'DNA-ss' for FASTA input, 'DNA-ds' for FASTQ input"),
+      (fun _ -> Parameters.content := Some (TA.get_parameter () |> Content.of_string));
+    [ "-f"; "--fasta" ],
       Some "<fasta_file_name>",
-      [ "FASTA input file containing sequences" ],
+      [ "FASTA input file containing sequences.";
+        "You can specify more than one FASTA input, but not FASTA and FASTQ inputs";
+        "at the same time. Contents are expected to be homogeneous across inputs" ],
       TA.Optional,
       (fun _ -> Files.Type.FASTA (TA.get_parameter ()) |> Tools.List.accum Parameters.inputs);
-    [ "-s"; "-S"; "--single-end" ],
+    [ "-s"; "--single-end" ],
       Some "<fastq_file_name>",
-      [ "FASTQ input file containing single-end sequencing reads" ],
+      [ "FASTQ input file containing single-end sequencing reads";
+        "You can specify more than one FASTQ input, but not FASTQ and FASTA inputs";
+        "at the same time. Contents are expected to be homogeneous across inputs" ],
       TA.Optional,
       (fun _ -> SingleEndFASTQ (TA.get_parameter ()) |> Tools.List.accum Parameters.inputs);
-    [ "-p"; "-P"; "--paired-end" ],
+    [ "-p"; "--paired-end" ],
       Some "<fastq_file_name1> <fastq_file_name2>",
-      [ "FASTQ input files containing paired-end sequencing reads" ],
+      [ "FASTQ input files containing paired-end sequencing reads";
+        "You can specify more than one FASTQ input, but not FASTQ and FASTA inputs";
+        "at the same time. Contents are expected to be homogeneous across inputs" ],
       TA.Optional,
       (fun _ ->
         let name1 = TA.get_parameter () in
@@ -185,19 +192,48 @@ let () =
       TA.Optional,
       (fun _ -> TA.usage (); exit 1)
   ];
+  if !Parameters.verbose then
+    TA.header ();
   Parameters.inputs := List.rev !Parameters.inputs;
   if !Parameters.inputs <> [] then begin
-    let store = ref Files.ReadsIterate.empty in
-    List.iter
-      (fun input -> store := Files.ReadsIterate.add_from_files !store input)
+    let is_format_fasta = ref false and store = ref Files.ReadsIterate.empty in
+    List.iteri
+      (fun i input ->
+        if i = 0 then
+          is_format_fasta := begin
+            match input with
+            | Files.Type.FASTA _ -> true
+            | SingleEndFASTQ _ | PairedEndFASTQ _ -> false
+            | _ -> assert false
+          end
+        else
+          if begin
+            match input with
+            | FASTA _ -> not !is_format_fasta
+            | SingleEndFASTQ _ | PairedEndFASTQ _ -> !is_format_fasta
+            | _ -> assert false
+          end then
+            TA.parse_error "You cannot process FASTA and FASTQ inputs together!";        
+        store := Files.ReadsIterate.add_from_files !store input)
       !Parameters.inputs;
+    if !Parameters.content = None then
+      Parameters.content := begin
+        if !is_format_fasta then
+          Some DNA_ss
+        else
+          Some DNA_ds
+      end;
     begin match !Parameters.content with
-    | DNA ->
-      let module KMCD = KMerCounter (KMers.DNAHashDoubleStranded (struct let value = !Parameters.k end)) in
-      KMCD.compute ~linter:(Sequences.Lint.dnaize ~keep_lowercase:false ~keep_dashes:false)
-    | Protein ->
-      let module KMCP = KMerCounter (KMers.ProteinHash (struct let value = !Parameters.k end)) in
-      KMCP.compute ~linter:(Sequences.Lint.proteinize ~keep_lowercase:false ~keep_dashes:false)
+    | None -> assert false
+    | Some DNA_ss ->
+      let module KMC = KMerCounter (KMers.DNAHashSingleStranded (struct let value = !Parameters.k end)) in
+      KMC.compute ~linter:(Sequences.Lint.dnaize ~keep_lowercase:false ~keep_dashes:false)
+    | Some DNA_ds ->
+      let module KMC = KMerCounter (KMers.DNAHashDoubleStranded (struct let value = !Parameters.k end)) in
+      KMC.compute ~linter:(Sequences.Lint.dnaize ~keep_lowercase:false ~keep_dashes:false)
+    | Some Protein ->
+      let module KMC = KMerCounter (KMers.ProteinHash (struct let value = !Parameters.k end)) in
+      KMC.compute ~linter:(Sequences.Lint.proteinize ~keep_lowercase:false ~keep_dashes:false)
     end ~verbose:!Parameters.verbose !store !Parameters.max_results_size !Parameters.label !Parameters.output
   end
 
